f319bd68251576bf21dece2b145e64c7b460de00
Fabio Di Sabatino <fabio_disabatino@hotmail.it>
2017-11-23 14:48:51 +0100

    Get Score
    Unit test passed for a game with only one strike

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -12,25 +12,53 @@ public class BowlingGame {
 	private void elaborateScore(String throwsResults) {
 		// TODO Auto-generated method stub
 		String [] arrayThrow= throwsResults.split("]");
-		for (String s : arrayThrow)
+		for (int i=0;i<10;i++)
 		{
+			String s=arrayThrow[i];
+			
+			//Call the parseThrow method in order to convert the attempt string
+			//in a pair of integer that represents how many pins were knocks down 
+			//in this throw
+			int[] attempt = this.parseThrow(s);
+			
+			int firstAttempt=attempt[0];
+			int secondAttempt=attempt[1];
+		
+			if(firstAttempt==10)
+			{
+				//we had a strike
+				int[] nextAttempt= this.parseThrow(arrayThrow[i+1]);
+				int firstNextAttempt=nextAttempt[0];
+				int secondNextAttempt=nextAttempt[1];
+				
+				this.score+=10+firstNextAttempt+secondNextAttempt;
+			}
+			else
+			{
+				this.score+=firstAttempt+secondAttempt;
+			}
 			
 			
-			//get how many pins were knock down in the first attempt of this throw
-			String temp=s.split(",")[0];
-			temp= temp.substring(1);
-			int firstAttempt=Integer.parseInt(temp);
-			
-			//get how many pins were knock down in the second attempt of this throw
-			int secondAttempt=Integer.parseInt(s.split(",")[1]);
-			
-			
-			
-			this.score+=firstAttempt+secondAttempt;
 			
 			
 		}
 		
+		
+	}
+	
+	private int[] parseThrow(String s)
+	{
+		int[] attempt= new int[2];
+		
+		//get how many pins were knock down in the first attempt of this throw
+		String temp=s.split(",")[0];
+		temp= temp.substring(1);
+		attempt[0]=Integer.parseInt(temp);
+		
+		//get how many pins were knock down in the second attempt of this throw
+		attempt[1]=Integer.parseInt(s.split(",")[1]);
+		
+		return attempt;
 		
 	}
 
